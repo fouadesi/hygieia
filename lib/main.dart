5e00023b37f7b357b22689eeb79543896fda7c43
fouadesi <83358349+djellalifouad@users.noreply.github.com>
2021-05-18 15:05:40 +0100

    add welcome pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+<<<<<<< Updated upstream
 import 'package:hygeia/slides/views/component/widget.page1.dart';
 import 'package:hygeia/users/controller/login.dart';
 import 'package:hygeia/users/view/auth/widget.login.dart';
+=======
+import 'package:hygeia/slides/views/widget.slide.dart';
+>>>>>>> Stashed changes
 
 void main() {
   runApp(MyApp());
@@ -13,12 +17,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // "hot reload" (press "r" in the console where you ran "flutter run",
+          // "hot reload" (press "r" in the console where you ran "flutter run",
         primarySwatch: Colors.blue,
-
       ),
       debugShowCheckedModeBanner: false,
+<<<<<<< Updated upstream
       home:  Login(),
+=======
+      home:  OnboardingScreen(),
+>>>>>>> Stashed changes
     );
   }
 }
