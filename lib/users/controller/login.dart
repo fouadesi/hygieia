6a2956deacc907b13c841b1515455181219c3cab
Merzouk-ilyes <i.merzouk@esi-sba.dz>
2021-05-24 19:01:44 +0100

    added error messages

--- lib/users/controller/login.dart
+++ lib/users/controller/login.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -10,18 +11,29 @@ login(String email, String pass, context) async {
   Map body = {"email": email, "password": pass};
   var jsonresponse;
   var res = await http.post(Uri.parse(url), body: body);
-  if (res.statusCode == 200) {
-    jsonresponse = jsonDecode(res.body);
-    print('Response status ${res.statusCode}');
-    print('Response body ${res.body}');
-    if (jsonresponse != null) {
-      sharedPrefrences.setString("token", jsonresponse['token']);
-      Navigator.pushAndRemoveUntil(
-          context,
-          MaterialPageRoute(
-              builder: (BuildContext context) =>
-                  Container(child: Text('Auth seccessful !'))),
-          (Route<dynamic> route) => false);
-    }
+
+  jsonresponse = jsonDecode(res.body);
+  print('Response status ${res.statusCode}');
+  print('Response body ${res.body}');
+
+  if (jsonresponse['token'] != null) {
+    sharedPrefrences.setString("token", jsonresponse['token']);
+    Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(
+            builder: (BuildContext context) =>
+                Container(child: Text('Auth seccessful !'),)),
+            (Route<dynamic> route) => false);
+  } else {
+    
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(
+      content: Text('${jsonresponse['message']}', style: GoogleFonts.archivo(
+        fontWeight: FontWeight.w500,
+        color: Colors.red,
+
+      ), ),
+      backgroundColor:Colors.white.withOpacity(0.6),
+    ));
   }
 }
